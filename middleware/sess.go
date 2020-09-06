fd6ae46c8a2feec2ef0b2285e4a3a9c3937edc3a
Vajahat Kareem <vajahatk@dotpe.in>
2020-09-06 19:52:17 +0530

    Update

--- middleware/sess.go
+++ middleware/sess.go
@@ -1,13 +1,15 @@
 package middleware
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/rs/xid"
 )
 
 func SessionManager(c *gin.Context) {
-	if sess, err := c.Cookie("_sessid"); err != nil || len(sess) == 0 {
-		c.SetCookie("_sessid", xid.New().String(), 2, "/", c.Request.URL.Host, true, false)
+	if sess, err := c.Cookie("sessid"); err != nil || len(sess) == 0 {
+		c.SetCookie("sessid", xid.New().String(), int(4*time.Hour), "/", c.Request.URL.Host, true, false)
 	}
 	c.Next()
 }
