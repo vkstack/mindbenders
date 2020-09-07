e99a56e9df40789840e1fe2fc5b1feb5013b0453
Vajahat Kareem <vajahatk@dotpe.in>
2020-09-07 23:04:33 +0530

    added base63 encoder/decoder

--- middleware/sess.go
+++ middleware/sess.go
@@ -8,7 +8,7 @@ import (
 )
 
 func SessionManager(c *gin.Context) {
-	if sess, err := c.Cookie("sessid"); err != nil || len(sess) == 0 {
+	if _, err := c.Cookie("sessid"); err != nil {
 		c.SetCookie("sessid", xid.New().String(), int(4*time.Hour), "/", c.Request.URL.Host, true, false)
 	}
 	c.Next()
