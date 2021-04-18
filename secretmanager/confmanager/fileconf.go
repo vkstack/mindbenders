b142617fd85451c972de60941d9bea0c4ae2f600
Divyesh <divyeshk@dotpe.in>
2021-04-18 19:11:20 +0530

    added secret manager support

---
+++ secretmanager/confmanager/fileconf.go
@@ -0,0 +1,49 @@
+package confmanager
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"os"
+	"path"
+
+	"gitlab.com/dotpe/mindbenders/secretmanager/config"
+)
+
+const devconfig = "config.local.json"
+
+//FileConfig It is for dev configs only.
+//For staging & prod SecretManager will be used.
+type fileConfig struct {
+	keyVal map[string]interface{}
+}
+
+func GetFileConfigManager() (config.IConfig, error) {
+	cfgMgr := fileConfig{}
+	localConfig, err := os.Open(path.Join(os.Getenv("cwd"), os.Getenv("CONFIGDIR"), devconfig))
+	if err != nil {
+		return nil, err
+	}
+	rawbytes, err := ioutil.ReadAll(localConfig)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(rawbytes, &cfgMgr.keyVal)
+	if err != nil {
+		return nil, err
+	}
+	//can have some preprocessing logic
+	return &cfgMgr, nil
+}
+
+func (cfgmgr *fileConfig) Get(key string) ([]byte, error) {
+	if val, ok := cfgmgr.keyVal[key]; ok {
+		byteVal, err := json.Marshal(val)
+		if err != nil {
+			return nil, errors.New("error in Unmarshal config")
+		}
+		return byteVal, nil
+	}
+	//handle this error very carefully
+	return nil, errors.New("no such config exists")
+}
---
+++ secretmanager/confmanager/secretManager.go
@@ -0,0 +1,61 @@
+package confmanager
+
+import (
+	"encoding/base64"
+	"fmt"
+	"strings"
+
+	"gitlab.com/dotpe/mindbenders/secretmanager/config"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
+)
+
+const awsRegion = "ap-south-1"
+
+type secretManager struct {
+	ENV string
+}
+
+// GetSecretManager ..
+func GetSecretManager(env string) config.IConfig {
+	//can  have some preprocessing logic
+	return &secretManager{
+		ENV: env,
+	}
+}
+
+func (cfgmgr *secretManager) getSearchKey(key string) string {
+	return cfgmgr.ENV + "/" + strings.Trim(key, "/")
+}
+
+//Create a Secrets Manager client
+func (cfgmgr *secretManager) Get(key string) ([]byte, error) {
+	newSession, _ := session.NewSession()
+	svc := secretsmanager.New(newSession, aws.NewConfig().WithRegion(awsRegion))
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId:     aws.String(cfgmgr.getSearchKey(key)),
+		VersionStage: aws.String("AWSCURRENT"), // VersionStage defaults to AWSCURRENT if unspecified
+	}
+	// In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+	// See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+	// var secretString *string
+	result, err := svc.GetSecretValue(input)
+	if err != nil {
+		return nil, err
+	}
+	// Decrypts secret using the associated KMS CMK.
+	// Depending on whether the secret is a string or binary, one of these fields will be populated.
+	if result.SecretString != nil {
+		return []byte(*result.SecretString), nil
+	}
+	decodedBinarySecretBytes := make([]byte, base64.StdEncoding.DecodedLen(len(result.SecretBinary)))
+	len, err := base64.StdEncoding.Decode(decodedBinarySecretBytes, result.SecretBinary)
+	if err != nil {
+		fmt.Println("Base64 Decode Error:", err)
+		return nil, err
+	}
+	return decodedBinarySecretBytes[:len], nil
+}
