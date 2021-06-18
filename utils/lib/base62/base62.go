af1d65f1e0905b08ceac826195f57f7ba08ea8aa
Vajahat Kareem <vajahatk@dotpe.in>
2021-06-18 14:36:23 +0530

    Hash collision fixed by droping float64 conversion we were losing someinfo while converting int64 to float64

--- utils/lib/base62/base62.go
+++ utils/lib/base62/base62.go
@@ -2,7 +2,6 @@ package base62
 
 import (
 	"errors"
-	"math"
 	"strings"
 )
 
@@ -18,42 +17,33 @@ var (
 // string of the given integer number.
 func Encode(num int64) string {
 	b := make([]byte, 0)
-
-	// loop as long the num is bigger than zero
 	for num > 0 {
-		// receive the rest
-		r := math.Mod(float64(num), float64(Base))
-
-		// devide by Base
+		r := int(num % Base)
 		num /= Base
-
-		// append chars
-		b = append([]byte{CharacterSet[int(r)]}, b...)
+		b = append([]byte{CharacterSet[r]}, b...)
 	}
-
 	return string(b)
 }
 
 // Decode returns a integer number of a base62 encoded string.
 func Decode(s string) (int64, error) {
 	var r int64
-	var pow int
-
-	// loop through the input
+	var power int
 	for i, v := range s {
-		// convert position to power
-		pow = len(s) - (i + 1)
-
-		// IndexRune returns -1 if v is not part of CharacterSet.
+		power = len(s) - (i + 1)
 		pos := strings.IndexRune(CharacterSet, v)
-
 		if pos == -1 {
 			return int64(pos), errors.New("invalid character: " + string(v))
 		}
-
-		// calculate
-		r += int64(pos) * int64(math.Pow(float64(Base), float64(pow)))
+		r += int64(pos) * pow(Base, int64(power))
 	}
-
 	return r, nil
+}
+
+func pow(base int64, exponent int64) int64 {
+	if exponent != 0 {
+		return (base * pow(base, exponent-1))
+	} else {
+		return 1
+	}
 }
