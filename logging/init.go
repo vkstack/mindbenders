9090ac0641604a45bb70b3d6ea5cef3fb6091087
Vajahat Kareem <vajahatk@dotpe.in>
2021-10-24 17:24:52 +0530

    logger is made fully configurable

--- logging/init.go
+++ logging/init.go
@@ -1,45 +1,30 @@
 package logging
 
 import (
-	"io/ioutil"
 	"os"
 	"sync"
 
-	"github.com/sirupsen/logrus"
 	"gitlab.com/dotpe/mindbenders/interfaces"
 )
 
-var lock = &sync.Mutex{}
+var lock sync.Mutex
 var logger interfaces.IDotpeLogger
+var host, _ = os.Hostname()
 
 //InitLogger sets up the logger object with LoeggerOptions provided.
 //It returns reference logger object and error
-func InitLogger(lops *LoggerOptions) (interfaces.IDotpeLogger, error) {
-	lops.Hostname, _ = os.Hostname()
+func Init(opts ...Option) interfaces.IDotpeLogger {
 	if logger == nil {
 		lock.Lock()
+		defer lock.Unlock()
 		if logger == nil {
-			if err := initlogger(lops); err != nil {
-				return nil, err
+			var dlogger = new(dlogger)
+			for _, opt := range opts {
+				opt(dlogger)
 			}
+			dlogger.setEssentials()
+			logger = dlogger
 		}
-		lock.Unlock()
 	}
-	return logger, nil
-}
-
-func initlogger(lops *LoggerOptions) error {
-	log := logrus.New()
-	log.SetNoLock()
-	hook, err := lops.IConfig.getHook()
-	if err != nil {
-		log.Panic(err)
-		return err
-	}
-	log.Hooks.Add(hook)
-	if lops.LOGENV != "dev" {
-		log.Out = ioutil.Discard
-	}
-	logger = &dlogger{Logger: log, Lops: *lops}
-	return nil
+	return logger
 }
---
+++ logging/option.go
@@ -0,0 +1,42 @@
+package logging
+
+import (
+	"io/ioutil"
+
+	"github.com/sirupsen/logrus"
+)
+
+type Option func(dlogger *dlogger)
+
+func WithAppInfo(app, env, wd string) Option {
+	return func(dlogger *dlogger) {
+		dlogger.app = app
+		dlogger.env = env
+		dlogger.wd = wd
+	}
+}
+
+func WithLogHook(hook logrus.Hook) Option {
+	return func(dlogger *dlogger) {
+		dlogger.logger = logrus.New()
+		dlogger.logger.SetNoLock()
+		dlogger.logger.Hooks.Add(hook)
+		if dlogger.env != "dev" {
+			dlogger.logger.Out = ioutil.Discard
+		}
+	}
+}
+
+func WithAccessLogOptions(opts ...accessLogOption) Option {
+	return func(dlogger *dlogger) {
+		dlogger.accopts = append(dlogger.accopts, accessLogOptionBasic(dlogger.app))
+		dlogger.accopts = append(dlogger.accopts, opts...)
+	}
+}
+
+func WithLogOptions(opts ...logOption) Option {
+	return func(dlogger *dlogger) {
+		dlogger.loptions = append(dlogger.loptions, logOptionBasic)
+		dlogger.loptions = append(dlogger.loptions, opts...)
+	}
+}
