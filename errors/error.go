0de76ce6bb4e4da06ef76f7656afed39723243aa
Vajahat Kareem <vajahatk@dotpe.in>
2021-11-17 11:09:37 +0530

    added new constructores for building error

--- errors/error.go
+++ errors/error.go
@@ -1,8 +1,6 @@
 package errors
 
-import "runtime"
-
-type withMessage struct {
+type base struct {
 	msg   string
 	cause error
 
@@ -11,61 +9,50 @@ type withMessage struct {
 	// stack []uintptr
 }
 
-func callers() []uintptr {
-	const depth = 32
-	var pcs [depth]uintptr
-	n := runtime.Callers(3, pcs[:])
-	return pcs[0:n]
-}
+// func callers() []uintptr {
+// 	const depth = 32
+// 	var pcs [depth]uintptr
+// 	n := runtime.Callers(3, pcs[:])
+// 	return pcs[0:n]
+// }
 
 var DefaultSeparator string = "\n"
 
 func New(msg string) error {
-	return &withMessage{msg: msg}
+	return &base{msg: msg}
 }
 
 func NewWithCode(msg string, code Code) error {
-	return &withMessage{msg: msg, code: code}
+	return &base{msg: msg, code: code}
 }
 
-func WrapCode(err error, code Code) error {
+func WrapMessage(err error, msg string) error {
 	if err == nil {
 		return nil
 	}
-	if e, ok := err.(*withMessage); ok {
-		return &withMessage{msg: err.Error(), cause: e, code: code}
-	}
-	return &withMessage{cause: err, code: code}
+	return &base{msg: msg, cause: err}
 }
 
-// func NewwithMessageFromNumbermsg(number int, msg string) error {
-// 	return &withMessage{msg: msg, errs: []string{msg}, errorNumber: number}
-// }
+func WrapMessageWithCode(err error, msg string, code Code) error {
+	if err == nil {
+		return nil
+	}
+	return &base{msg: msg, cause: err, code: code}
+}
 
-// Replace you errors.New("") to N
-// func Wrap(err error, msg string) error {
-// 	if e, ok := err.(*withMessage); ok {
-// 		return &withMessage{msg: msg, cause: e}
-// 	}
-// 	return &withMessage{msg: msg, errs: []error{err}}
-// }
-
-// this will give entire error trace
-// It is a thread unsafe method
-
-func (e *withMessage) String() string {
+func (e *base) String() string {
 	return e.msg
 }
 
-func (e *withMessage) Error() string {
+func (e *base) Error() string {
 	return e.msg + DefaultSeparator + e.cause.Error()
 }
 
-func (e *withMessage) Cause() error {
+func (e *base) Cause() error {
 	return e.cause
 }
 
-func (e *withMessage) Code() Code {
+func (e *base) Code() Code {
 	return e.code
 }
 
