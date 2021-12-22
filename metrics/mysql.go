6afefdd1300580c9493ea83ff6b40b8edb1e05ea
Vajahat Kareem <vajahatk@dotpe.in>
2021-12-22 12:46:25 +0530

    db metrics prom

--- metrics/mysql.go
+++ metrics/mysql.go
@@ -2,6 +2,7 @@ package metrics
 
 import (
 	"database/sql"
+	"sync"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -10,20 +11,35 @@ import (
 
 const defaultDuration = 5 * time.Second
 
-func WithDBPoolMetrics(db interface{ Stats() sql.DBStats }, d time.Duration, lablevalue map[string]string) {
-	if d.Seconds() > 5 {
+var dbcollector *prometheus.CounterVec
+
+func dbpoolcollector() {
+	dbcollector = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name:      "conn_wait_count",
+		Subsystem: "dotpe",
+		Namespace: "mysql",
+		Help:      "Mysql Pool Connection request wait count",
+	}, []string{"db"})
+}
+
+type DBCollectorLables struct {
+	//db type... ie. payment-master | payment-slave
+	Type string
+}
+
+var oncedbpoolcollectorInit sync.Once
+
+func WithDBPoolMetrics(db interface{ Stats() sql.DBStats }, d time.Duration, lablevalue DBCollectorLables) {
+	oncedbpoolcollectorInit.Do(dbpoolcollector)
+	if d.Seconds() < 10 {
 		d = defaultDuration
 	}
 	var waitCount int64
-	v1 := promauto.NewCounterVec(prometheus.CounterOpts{
-		Name: "mysql_conn_wait_count",
-		Help: "",
-	}, nil)
 	go func() {
 		for {
 			time.Sleep(d)
 			stats := db.Stats()
-			v1.With(lablevalue).Add(float64(stats.WaitCount - waitCount))
+			dbcollector.WithLabelValues(lablevalue.Type).Add(float64(stats.WaitCount - waitCount))
 			waitCount = stats.WaitCount
 		}
 	}()
