449caa75a69d4bfe72fde21bd50069cb882fae91
Vajahat Kareem <vajahatk@dotpe.in>
2023-03-20 21:43:49 +0530

    removed unuse handlers

--- middleware/gin/sess.go
+++
@@ -1,8 +0,0 @@
-package gin
-
-// func SessionManager(c *gin.Context) {
-// 	if _, err := c.Cookie("sessid"); err != nil {
-// 		c.SetCookie("sessid", xid.New().String(), int(4*time.Hour), "/", c.Request.URL.Host, true, false)
-// 	}
-// 	c.Next()
-// }
